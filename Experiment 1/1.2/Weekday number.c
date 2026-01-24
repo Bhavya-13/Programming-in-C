44eb78b6c1fd9968653006b809a87bde4fa9a600
Bhavya Agrawal <71514886+Bhavya-13@users.noreply.github.com>
2026-01-24 15:17:41 +0530

    Add weekday number program in C

---
+++ Experiment 1/1.2/Weekday number.c
@@ -0,0 +1,34 @@
+// Type Content here...
+#include<stdio.h>
+void main(){
+	int number;
+	//printf("Enter The number :- \n");
+	scanf("%d",&number);
+
+	switch(number){
+		case 0 : printf("Sunday\n");
+		break;
+
+		case 1 : printf("Monday\n");
+		break;
+
+		case 2 : printf("Tuesday\n");
+		break;
+
+		case 3 : printf("Wednesday\n");
+		break;
+
+		case 4 : printf("Thursday\n");
+		break;
+
+		case 5 : printf("Friday\n");
+		break;
+
+		case 6 : printf("Saturday\n");
+		break;
+
+		default:printf("Invalid weekday number\n");
+	}
+}
+
+
